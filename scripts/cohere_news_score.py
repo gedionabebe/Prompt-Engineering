b510c4ce568dfbf082d33bf1640c8393ce4ee850
Gedion <gedionandme@gmail.com>
2022-09-17 16:05:41 +0300

    News item score API
    - Implemented news item scoring API where the API will take a news prompt and return a score.

--- scripts/cohere_news_score.py
+++ scripts/cohere_news_score.py
@@ -4,7 +4,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 import cohere,logging
 logging.basicConfig(filename='../log/log.log', filemode='a',encoding='utf-8', level=logging.DEBUG)
-def cohere_classify(api_key,news):
+def cohere_classify(api_key,news,prompt):
     co = cohere.Client(api_key)
     sentences_train, sentences_test, labels_train, labels_test = train_test_split(list(news['Body']),list(news['Analyst_Rank']), test_size=0.2)
     embeddings_train = co.embed(texts=sentences_train,
@@ -13,9 +13,13 @@ def cohere_classify(api_key,news):
     embeddings_test = co.embed(texts=sentences_test,
                              model="large",
                              truncate="LEFT").embeddings
+    embeddings_prompt = co.embed(texts=prompt,
+                             model="large",
+                             truncate="LEFT").embeddings
     svm_classifier = make_pipeline(StandardScaler(), SVC(class_weight='balanced')) 
     svm_classifier.fit(embeddings_train, labels_train)
     score = svm_classifier.score(embeddings_test, labels_test)
-    prediction = svm_classifier.predict(labels_test)
+    prediction = svm_classifier.predict(embeddings_test)
+    prompt_prediction = svm_classifier.predict(embeddings_prompt)
 
-    return prediction, score
+    return prediction, score, prompt_prediction[0]
